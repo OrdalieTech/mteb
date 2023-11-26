c3c3a6bae7e75cd286fd5555a135ece34f07d46f
Baudouin A <39877685+netapy@users.noreply.github.com>
2023-11-26 09:22:04 +0100

    Add MiraclFRRerank class + fix

--- mteb/tasks/Reranking/OrdalieFRReranking.py
+++ mteb/tasks/Reranking/OrdalieFRReranking.py
@@ -1,7 +1,7 @@
 from ...abstasks.AbsTaskReranking import AbsTaskReranking
 
 
-class AskUbuntuDupQuestions(AbsTaskReranking):
+class OrdalieFRReranking(AbsTaskReranking):
     @property
     def description(self):
         return {
@@ -9,6 +9,22 @@ class AskUbuntuDupQuestions(AbsTaskReranking):
             "hf_hub_name": "OrdalieTech/Ordalie-FR-Reranking-benchmark",
             "description": (
                 "French queries and sentences."
+            ),
+            "type": "Reranking",
+            "category": "s2s",
+            "eval_splits": ["test"],
+            "eval_langs": ["fr"],
+            "main_score": "map",
+        }
+
+class MiraclFRRerank(AbsTaskReranking):
+    @property
+    def description(self):
+        return {
+            "name": "MiraclFRRerank",
+            "hf_hub_name": "OrdalieTech/MIRACL-FR-Reranking-benchmark",
+            "description": (
+                "French queries and sentences from the MIRACL dev dataset."
             ),
             "type": "Reranking",
             "category": "s2s",
