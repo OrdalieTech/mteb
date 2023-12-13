79edb7ccac7f3a07ecf6a2b58a40c724ccb13f76
Baudouin A <39877685+netapy@users.noreply.github.com>
2023-12-13 09:19:34 +0100

    Update model and evaluation in evaluate_model.py

--- examples/evaluate_model.py
+++ examples/evaluate_model.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO)
 
 model_name = "OrdalieTech/Solon-embeddings-base-0.1"
 
-model = SentenceTransformer(model_name)
+model = SentenceTransformer(model_name, device="mps")
 
 def encode_queries(self, queries, batch_size=2):
     modified_queries = ["query : " + query for query in queries]
@@ -18,7 +18,7 @@ model.encode_queries = encode_queries.__get__(model)
 model.encode_corpus = encode_corpus.__get__(model)
 
 # Run evaluation
-evaluation = MTEB(task_langs=["fr"]) # , tasks=["MSMarcoFRRetrieval"])
-evaluation.run(model, output_folder=f"results/{model_name}", eval_splits=["test"])
+evaluation = MTEB(task_langs=["fr"], tasks=["OrdalieLegalReranking"])
+evaluation.run(model, output_folder=f"results/{model_name}", eval_splits=["test"], verbosity=2)
 
 print("--DONE--")
--- mteb/tasks/Reranking/OrdalieFRReranking.py
+++ mteb/tasks/Reranking/OrdalieFRReranking.py
@@ -1,4 +1,5 @@
 from ...abstasks.AbsTaskReranking import AbsTaskReranking
+from datasets import load_dataset, DatasetDict
 
 
 class OrdalieFRReranking(AbsTaskReranking):
@@ -17,6 +18,7 @@ class OrdalieFRReranking(AbsTaskReranking):
             "main_score": "map",
         }
 
+
 class MiraclFRRerank(AbsTaskReranking):
     @property
     def description(self):
@@ -32,3 +34,25 @@ class MiraclFRRerank(AbsTaskReranking):
             "eval_langs": ["fr"],
             "main_score": "map",
         }
+
+class OrdalieLegalReranking(AbsTaskReranking):
+    @property
+    def description(self):
+        return {
+            "name": "OrdalieLegalReranking",
+            "hf_hub_name": "OrdalieTech/solon-legal",
+            "description": (
+                "French legal sentences and sentences."
+            ),
+            "type": "Reranking",
+            "category": "s2s",
+            "eval_splits": ["test"],
+            "eval_langs": ["fr"],
+            "main_score": "map",
+        }
+
+    def load_data(self, **kwargs):
+        if not self.data_loaded:
+            dataset = load_dataset(self.description['hf_hub_name'])
+            self.dataset = dataset.map(lambda e: {'query': e['query'], 'positive': e['pos'], 'negative': e['neg']})
+            self.data_loaded = True
